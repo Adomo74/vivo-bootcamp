a7cd5c47f0a0f9df15a445f53585da8fb5ae30e7
adomo <steeljaw2006@gmail.com>
2024-06-07 21:15:26 -0300

    versão_final

--- DIO_VIVO/entendendo o desafio1 - dio.py
+++ DIO_VIVO/entendendo o desafio1 - dio.py
@@ -1,50 +1,47 @@
+conta = 0
+tentativas = [0, 1, 2]
+transacoes = []
 
+while True:
+    print("\nBem vindo ao sistema bancário da empresa DIO")
+    ope = input("""
+             Selecione qual operação quer fazer:
+             Depósito   (D)
+             Saque      (S)
+             Extrato    (E)
+             Fechar     (F)
+    
+             """).strip().upper()
 
-def banco():
-
-
-    conta = 0
-    esc = True
-    while esc == True:
-        print("\nBem vindo ao sistema bancário da empresa DIO")
-        ope = input("""Selecione qual operação quer fazer:
-         Depósito   (D)
-         Saque      (S)
-         Extrato    (E)
-         Fechar     (F)
-        
-         """).strip().upper()
-
-
-        if ope == "D":
-
-            def deposito():
-                global conta
-                vdp = int(input("Qual o valor do depósito? "))
-                if vdp >= 0:
-                    conta += vdp
-                    print(conta)
-                else:
-                    print("Operação inválida. O valor do depósito deve ser maior ou igual a zero.")
-
-            deposito()
-
-
-
+    if ope == "D":
+        vdp = int(input("Qual o valor do depósito? "))
+        if vdp >= 0:
+            conta += vdp
+            print(f"Você depositou R${vdp}, e agora sua conta está no total com R${conta}")
+            transacoes.append({"tipo": "Depósito", "valor": vdp})
+        else:
+            print("Operação inválida. O valor do depósito deve ser maior ou igual a zero.")
 
     if ope == "S":
-        #tentativas = [1 + 0]
-        saq = int(input("Qual o valor do saque?"))
-        dsaq = saq - conta
-        print(conta)
-        #while tentativas == 3:
-            #print("chegou o limite de operações")
+        vds = int(input("Qual valor do saque?"))
+
+        for vezes in tentativas:
+            if vezes == 2:
+                print("Você chegou ao número maximo de saques, por favor reinicie o programa")
+                quit()
+        if vds >= conta:
+            conta -= vds
+            print(f"Você sacou R${vds} e agora sua conta está com R${conta}")
+
+        if conta < 0:
+            print(
+                "Você não pode retirar esse valor, pois ele não existe, logo estará devendo e será descontado no "
+                "próximo depósito")
+            transacoes.append({"tipo": "Saque", "valor": vds})
 
     if ope == "E":
-        print("DEU BOM")
+        for t in transacoes:
+            print(f"{t["tipo"]}: R${t['valor']}")
 
-    if ope == "F":
-        esc = False
-
-
-banco()
+        if ope == "F":
+            pass
