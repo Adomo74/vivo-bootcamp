dae858649cd69051ba69b40fc0e52437270c9dc2
adomo <steeljaw2006@gmail.com>
2024-06-26 23:35:53 -0300

    segunda parte 26/06

--- DIO_VIVO/entendendo o desafio1 - dio.py
+++ DIO_VIVO/entendendo o desafio1 - dio.py
@@ -1,9 +1,85 @@
 conta = 0
 tentativas = [0, 1, 2]
 transacoes = []
+T = True
 
-while True:
-    print("\nBem vindo ao sistema bancário da empresa DIO")
+
+def cadstro_pessoa():
+    nome = input("Qual seu nome?").strip()
+    dtn = input("Qual sua data de nascimento?").strip()
+    cpf = input("Qual seu cpf?").strip()
+
+    logradouro = input("Qual seu logradouro?").strip()
+    nro = input("Qual seu número?").strip()
+    bairro = input("Qual o seu bairro?").strip()
+    Cidade = input("Qual sua cidade?").strip()
+    Estado = input("Qual o Estado? Em sigla por favor").strip()
+
+    endereço = {logradouro, nro, bairro, Cidade, Estado}
+
+    Pessoa = {nome, dtn, cpf, endereço}
+    print(Pessoa)
+
+
+cadstro_pessoa()
+
+
+def criando_conta_corrente():
+    pass
+
+
+def D():
+    global conta
+    vdp = int(input("Qual o valor do depósito? "))
+    if vdp >= 0:
+        conta += vdp
+        print(f"Você depositou R${vdp}, e agora sua conta está no total com R${conta}")
+        transacoes.append({"tipo": "Depósito", "valor": vdp})
+    else:
+        print("Operação inválida. O valor do depósito deve ser maior ou igual a zero.")
+
+    return conta
+
+
+def S():
+    global conta
+
+    vds = int(input("Qual valor do saque?"))
+
+    for vezes in tentativas:
+        if vezes == 2:
+            print("Você chegou ao número maximo de saques, por favor reinicie o programa")
+            quit()
+    if vds >= conta:
+        conta -= vds
+        print(f"Você sacou R${vds} e agora sua conta está com R${conta}")
+
+    if conta < 0:
+        print(
+            "Você não pode retirar esse valor, pois ele não existe, logo estará devendo e será descontado no "
+            "próximo depósito")
+        transacoes.append({"tipo": "Saque", "valor": vds})
+
+        return conta
+
+
+def E():
+    for t in transacoes:
+        print(f"{t["tipo"]}: R${t['valor']}")
+
+    if ope == "F":
+        pass
+
+
+def F():
+    global T
+    T = False
+    print(f"O programa encerra aqui {conta}, tenha um bom dia!")
+    return T
+
+
+while False != T:
+    print("\nBem vindo {}, ao sistema bancário da empresa DIO")
     ope = input("""
              Selecione qual operação quer fazer:
              Depósito   (D)
@@ -14,34 +90,13 @@ while True:
              """).strip().upper()
 
     if ope == "D":
-        vdp = int(input("Qual o valor do depósito? "))
-        if vdp >= 0:
-            conta += vdp
-            print(f"Você depositou R${vdp}, e agora sua conta está no total com R${conta}")
-            transacoes.append({"tipo": "Depósito", "valor": vdp})
-        else:
-            print("Operação inválida. O valor do depósito deve ser maior ou igual a zero.")
+        D()
 
     if ope == "S":
-        vds = int(input("Qual valor do saque?"))
-
-        for vezes in tentativas:
-            if vezes == 2:
-                print("Você chegou ao número maximo de saques, por favor reinicie o programa")
-                quit()
-        if vds >= conta:
-            conta -= vds
-            print(f"Você sacou R${vds} e agora sua conta está com R${conta}")
-
-        if conta < 0:
-            print(
-                "Você não pode retirar esse valor, pois ele não existe, logo estará devendo e será descontado no "
-                "próximo depósito")
-            transacoes.append({"tipo": "Saque", "valor": vds})
+        S()
 
     if ope == "E":
-        for t in transacoes:
-            print(f"{t["tipo"]}: R${t['valor']}")
+        E()
 
-        if ope == "F":
-            pass
+    if ope == "F":
+        F()
